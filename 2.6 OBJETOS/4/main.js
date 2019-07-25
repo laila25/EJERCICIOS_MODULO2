f13a53ee363f22feeb913e195f4ac3e335a1f772
laila25 <laila2519@gmail.com>
2019-07-25 20:12:52 +0200

    add

--- 2.6 OBJETOS/4/main.js
+++ 2.6 OBJETOS/4/main.js
@@ -3,7 +3,7 @@
 const button = document.querySelector(".btn");
 
 function showButton(event) {
-  console.log(event.currentTarget);
+  console.log(event);
 }
 
 button.addEventListener("click", showButton);
